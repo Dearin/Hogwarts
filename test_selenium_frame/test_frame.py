240633026e883913019958921c6a9d4a3664029a
Dearin <987221507@qq.com>
2020-10-26 15:47:55 +0800

    更新selenium多窗口和frame

---
+++ test_selenium_frame/test_frame.py
@@ -0,0 +1,29 @@
+from selenium.webdriver import ActionChains
+
+from test_selenium_frame.base import Base
+from time import sleep
+
+class TestFrame(Base):
+
+    def test_frames(self):
+        url = "https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable"
+        self.driver.get(url)
+
+        # 首先要进入frame
+        self.driver.switch_to.frame('iframeResult')
+        drag_ele = self.driver.find_element_by_xpath('//*[@id="draggable"]')
+        drop_ele = self.driver.find_element_by_xpath('//*[@id="droppable"]')
+
+        print(drag_ele.text)
+        # 这里注释的话，测试页面会出现弹窗，导致最后一句失败被阻塞
+        # action = ActionChains(self.driver)
+        # action.drag_and_drop(drag_ele,drop_ele).perform()
+        sleep(2)
+        # 再回到正常的页面中
+
+        self.driver.switch_to.default_content()
+        # 打印正常页面中的
+        print(self.driver.find_element_by_xpath('//*[@id="submitBTN"]').text)
+        sleep(2)
+
+
---
+++ test_selenium_frame/test_windows.py
@@ -0,0 +1,50 @@
+from test_selenium_frame.base import Base
+from time import sleep
+
+class TestWindows(Base):
+
+    def test_windows(self):
+        '''
+        进入首页，点击右上角登陆，弹出登陆弹窗
+        点击弹窗中的立即注册，跳转至注册页面，输入注册信息后
+        返回上一页面，点击登陆 - 用户登陆
+
+        '''
+        url = "https://www.baidu.com"
+        self.driver.get(url)
+        sleep(1)
+        login_ele = self.driver.find_element_by_xpath('//*[@id="u1"]/a')
+        login_ele.click()
+        print(self.driver.current_window_handle)
+        sleep(2)
+
+       # 定位到立即注册，并点击跳转
+        register_ele = self.driver.find_element_by_link_text("立即注册")
+        register_ele.click()
+        print(self.driver.window_handles)
+        print(self.driver.current_window_handle)
+        windows = self.driver.window_handles
+        sleep(3)
+
+       # 输入相关信息，强制等待3s
+        self.driver.switch_to_window(windows[-1])
+        name_ele = self.driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_4__userName"]')
+        phonenum_ele = self.driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_4__phone"]')
+        name_ele.send_keys("username")
+        phonenum_ele.send_keys("13400000001")
+        sleep(3)
+
+        #返回首页
+        self.driver.switch_to_window(windows[0])
+        sleep(2)
+        # 选择用户名登陆
+        self.driver.find_element_by_id('TANGRAM__PSP_11__footerULoginBtn').click()
+        sleep(2)
+
+
+
+
+
+
+
+
