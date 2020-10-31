f80d8d59b50f96d8a9315fded90a05ea7c95940c
Dearin <987221507@qq.com>
2020-10-31 22:42:21 +0800

    更新 了点

--- Appium_Demo/appium_webview/test_wechat_program.py
+++ Appium_Demo/appium_webview/test_wechat_program.py
@@ -1,7 +1,10 @@
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
 from time import sleep
 
 from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestWechatProgram:
@@ -20,9 +23,9 @@ class TestWechatProgram:
         desire_caps["resetKeyBoard"] = "true"  # 输入中文字符
         desire_caps["browser"] = "Chrome"  # 输入中文字符
         desire_caps["ShowChromedriverLog"] = "true"  # 输入中文字符
-        desire_caps["chromedriverExecutableDir"] = "/Users/deng/Documents/driver/chromedriver"  # 输入中文字符
+        # desire_caps["chromedriverExecutableDir"] = "/Users/deng/Documents/driver/chromedriver"  # 输入中文字符
 
-       # 将chromeoption传递给chromedriver
+        # 将chromeoption传递给chromedriver
         desire_caps["chromeOptions"] = {
             'androidProcess': 'com.tencent.mm:appbrand0'
         }
@@ -30,11 +33,6 @@ class TestWechatProgram:
         desire_caps["adbPort"] = 5037
         # 初始化驱动
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desire_caps)
-        # 设置一个隐形等待，增加代码的健壮性
-        self.driver.implicitly_wait(5)
-
-        self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']")
-        self.driver.implicitly_wait(10)
 
         # self.enter_micro_program()
         # print(self.driver.contexts)
@@ -42,12 +40,21 @@ class TestWechatProgram:
     def teardown(self):
         self.driver.quit()
 
-    def test_wechat_xueqiu(self):
+    def test_wechat_scroll(self):
         # 原生测试：滑动页面，展示出小程序搜索位置
         # 输入需要查询的小程序名称进行查找
         # 点击所需要的小程序，进入程序页面
-        # 小程序的测试：
-        # 在小程序中进行搜索和跳转
         size = self.driver.get_window_size()
-        self.driver.swipe(size['width'] * 0.3,size['height'] * 0.5,size['width'] * 0.3,size['height'] * 0.9)
+        print(self.driver.contexts)
+        # self.driver.swipe(size['width'] * 0.3,size['height'] * 0.5,size['width'] * 0.3,size['height'] * 0.9)
+        action = TouchAction(self.driver)
+        action.press(x =size['width'] * 0.3,y =size['height'] * 0.3).wait(100).move_to(x=size['width'] * 0.3,y=size['height'] * 0.9).wait(100).release().perform()
         sleep(3)
+        print(self.driver.contexts)
+        sleep(2)
+
+        # 切换都最顶部的页面
+        # self.driver.switch_to.context(self.driver.contexts[-1])
+        # self.driver.find_element_by_xpath("//android.widget.EditText[@resource-id='com.tencent.mm:id/lj']").click()
+
+
