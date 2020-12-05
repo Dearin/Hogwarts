635f8a74921ffdf5c9b7d2757e7f587f8e660ef4
Dearin <987221507@qq.com>
2020-12-05 12:02:51 +0800

    加了conftest.py文件

--- Services/testcases/conftest.py
+++ Services/testcases/conftest.py
@@ -15,3 +15,5 @@ def pytest_collection_modifyitems(
     for item in items:
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf8').decode('unicode-escape')
+        # 通过对items列表中的元素进行操作，就可以控制执行用例的顺序来
+    print("收集到的用例：%s"% items)
